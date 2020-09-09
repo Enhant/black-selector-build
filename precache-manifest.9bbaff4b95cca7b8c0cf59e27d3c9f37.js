657cf512bb2cd59e872b44e80c5929c05f1c2e20
Daniel <daniel.kuzin.sn@gmail.com>
2020-09-09 22:22:13 +0200

    builded version

---
+++ precache-manifest.9bbaff4b95cca7b8c0cf59e27d3c9f37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cfb375276a4699301306edbfad861a",
+    "url": "/black-selector/index.html"
+  },
+  {
+    "revision": "5de4870c947621e20ac1",
+    "url": "/black-selector/static/js/2.cd032eb3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/black-selector/static/js/2.cd032eb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0eb1f78f75b55ce3d04",
+    "url": "/black-selector/static/js/main.20541cc3.chunk.js"
+  },
+  {
+    "revision": "202c68003e8ad54084c3",
+    "url": "/black-selector/static/js/runtime-main.8917cff1.js"
+  }
+]);
